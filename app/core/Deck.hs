75e3b51dd61bd1cd62f2052e93557d6d0e5fa450
Douglas Domingos <doug.domingossilva@gmail.com>
2024-03-24 14:01:32 -0300

    feat: adiciona função playCard

--- app/core/Deck.hs
+++ app/core/Deck.hs
@@ -1,16 +1,32 @@
 {-
- - Módulo referente às funcionalidades relacionadas ao Deck de cartas do jogo.
+    Módulo referente às funcionalidades relacionadas ao Deck de cartas do jogo.
  -}
 
 module Deck (
-    pushToEnd
+    playCard
 ) where
 
--- | Esta função pega uma carta específica do Deck e move até o final da lista
-pushToEnd :: Int -> [a] -> [a]
+import Card
+
+-- | Esta função recebe o índice da carta jogada e retorna o Deck
+-- após a jogada ser feita (com a carta jogada no final)
+playCard :: [Card] -> Int -> [Card]
+playCard cards index =
+    if validateIndex index
+        then pushToEnd index cards
+        else []
+
+-- | Esta função recebe o índice de uma carta do Deck e retorna o
+-- Deck com a carta selecionada na última posição
+pushToEnd :: Int -> [Card] -> [Card]
 pushToEnd _ [] = []
 pushToEnd _ [x] = [x]
 pushToEnd index xs
     | index < 0 || index >= length xs = xs
-    | otherwise = fst list ++ tail(snd list) ++ (head(snd list)):[]
+    | otherwise = fst list ++ tail (snd list) ++ [head (snd list)]
         where list = splitAt index xs
+
+-- | Essa função valida o `index` passado pelo jogador para selecionar uma carta.  
+-- O `index` será considerado válido se `0 <= index < 5`
+validateIndex :: Int -> Bool
+validateIndex index = index >= 0 && index < 5
