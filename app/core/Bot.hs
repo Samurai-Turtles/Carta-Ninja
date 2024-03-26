4e455449282f0240e104c66eff448d30213af6d5
losout0 <leandro.oliveira.souto@ccc.ufcg.edu.br>
2024-03-26 15:27:28 -0300

    Comentários

--- app/core/Bot.hs
+++ app/core/Bot.hs
@@ -7,7 +7,7 @@ module Bot where
 import Card
 import System.Random (randomRIO)
 
-
+-- Faz a seleção da carta
 makechoice :: [Card] -> [Int] -> Card
 make_choice hand weight = do
     let w_hand = weight_hand 0 hand weight
@@ -15,9 +15,10 @@ make_choice hand weight = do
 
     return (hand !! (randomRIO (0, len - 1)))
 
+-- Gera um array com as cartas baseado no array de pesos. Se o peso de uma carta é 5, terão 5 ocorrências dessa carta no array
 weight_hand :: Int -> [Card] -> [Int] -> [Int]
 weight_hand i [] [] = []
-weight_hand 5   = []
+weight_hand 5 _ _ = []
 weight_hand i cards weights =
     case elem of
         "F" -> replicate (weights !! 0) i ++ weight_hand (i + 1) cards weights
@@ -27,6 +28,7 @@ weight_hand i cards weights =
         "T" -> replicate (weights !! 4) i ++ weight_hand (i + 1) cards weights
   where elem = element (cards !! i)
 
+-- Retorna um deck com os mesmos elementos do deck passado, mas em uma ordem aleatória
 shuffle :: [a] -> IO [a]
 shuffle [] = []
 shuffle xs = do
@@ -36,6 +38,7 @@ shuffle xs = do
     let val = xs !! idx
     return ([val] ++ shuffle (removeFirstEqualTo val xs))
 
+-- Remove a primeira aparição do elemento passado do deck
 removeFirstEqualTo :: Eq a => a -> [a] -> [a]
 removeFirstEqualTo  [] = []
 removeFirstEqualTo x (y:ys)
