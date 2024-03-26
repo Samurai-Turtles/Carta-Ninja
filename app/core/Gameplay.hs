4e9ad8594f9f2370b237b50d96ea538bea637cf1
ViniciusUltraNX <115042218+ViniciusUltraAD@users.noreply.github.com>
2024-03-26 18:05:29 -0300

    feat: add changes to updateScoreOf and function to verify a victory
    Now updateScoreOf updates CPU and Player Streak correctly, setting one
    to zero when the other gets a win and the array of wins by element is
    modified too, verifyVictory is a function that verifies all victory
    conditions after the battle.json data.

--- app/core/Gameplay.hs
+++ app/core/Gameplay.hs
@@ -1,6 +1,7 @@
 {-
     Módulo referente às funcionalidades da Gameplay do jogo
 -}
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 
 module Gameplay where
 
@@ -57,8 +58,9 @@ udpatePlayerCampaignScore points = do
             
     writeCampaignState modifiedData
 
--- | Esta função recebe um valor inteiro indicando o vencedor da rodada, 
--- a carta vencedora e retorna a soma da pontuação atual com o poder da carta
+-- | Esta função recebe um valor inteiro indicando o vencedor da rodada 
+-- (1 indica que o jogador venceu e -1 indica que o bot venceu), a carta vencedora
+-- e retorna a soma da pontuação atual com o poder da carta
 updateScoreOf :: Int -> Card -> IO()
 updateScoreOf winner card = do
     -- TODO utilização: ao executar (no main?), chamar com a função getWinner,
@@ -66,24 +68,64 @@ updateScoreOf winner card = do
 
     let modifiedPlayerScore = if winner == 1 then power card else 0
     let modifiedCPUScore = if winner == -1 then power card else 0
-    
-    let modifiedPlayerWins = if winner == 1 then 1 else 0
-    let modifiedCPUWins = if winner == -1 then 1 else 0
-    
+       
     currentData <- getBattleState
+
+    let modifiedPlayerStreak = if winner == 1 then 1 + playerStreak currentData else 0
+    let modifiedCPUStreak = if winner == -1 then 1 + cpuStreak currentData else 0
+
+    let playerElementWin = if winner == 1 then modifyElemWinArray card (playerWinsByElement currentData) 
+                           else playerWinsByElement currentData
+    let cpuElementWin = if winner == -1 then modifyElemWinArray card (cpuWinsByElement currentData)
+                        else cpuWinsByElement currentData
+
     let modifiedData = 
             BattleState { 
                 currentRound = currentRound currentData,
 
                 playerScore = playerScore currentData + modifiedPlayerScore,
-                playerWins = playerWins currentData + modifiedPlayerWins, 
-                playerWinsByElement = playerWinsByElement currentData,
+                playerStreak = modifiedPlayerStreak, 
+                playerWinsByElement = playerElementWin,
                 playerDeck = playerDeck currentData,
 
                 cpuScore = cpuScore currentData + modifiedCPUScore,
-                cpuWins = cpuWins currentData + modifiedCPUWins, 
-                cpuWinsByElement = cpuWinsByElement currentData,
+                cpuStreak = modifiedCPUStreak, 
+                cpuWinsByElement = cpuElementWin,
                 cpuDeck = cpuDeck currentData
             }
                           
     writeBattleState modifiedData
+
+-- | Esta função verifica com os dados atuais do Battle State se algum lutador ganhou,
+-- com o valor 1 definindo a vitória do jogador, valor -1 definindo a vitória do bot e
+-- com o valor 0 dando a ideia de indefinição da vitória.
+verifyVictory :: BattleState -> Int
+verifyVictory battleState 
+    | playerStreak battleState == 3 = 1
+    | playerWinsByElement battleState == [True, True, True, True, True] = 1
+    | cpuStreak battleState == 3 = -1
+    | cpuWinsByElement battleState == [True, True, True, True, True] = -1
+    | currentRound battleState == 11 && playerScore battleState > cpuScore battleState = 1
+    | currentRound battleState == 11 && playerScore battleState < cpuScore battleState = -1
+    | otherwise = 0
+
+-- | Esta função recebe uma Carta e um array de booleanos que significa as vitórias
+-- de elementos na luta em questão e modifica esse array, atualizando esses valores
+-- booleanos para o estado correto.
+modifyElemWinArray :: Card -> [Bool] -> [Bool]
+modifyElemWinArray card winArray
+    | element card == "fire" = replaceAtIndex winArray True 0
+    | element card == "water" = replaceAtIndex winArray True 1
+    | element card == "nature" = replaceAtIndex winArray True 2
+    | element card == "metal" = replaceAtIndex winArray True 3
+    | element card == "earth" = replaceAtIndex winArray True 4
+
+-- | Função que recebe um array de um certo tipo e modifica o valor do índice dado
+-- por um inteiro, pelo valor dado na chamada logo após o array original, retornando
+-- essa cópia modificada do array original.
+replaceAtIndex :: [a] -> a -> Int -> [a]
+replaceAtIndex [] _ _ = []
+replaceAtIndex (h:t) item indx
+    | indx == 0 = item : t
+    | indx <= 0 = (h:t)
+    | otherwise = h : replaceAtIndex t item (indx - 1)
