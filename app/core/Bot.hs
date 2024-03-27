e9606317fc4db29b65e7aadd81e0b2fc19dcbea6
losout0 <leandro.oliveira.souto@ccc.ufcg.edu.br>
2024-03-27 19:45:11 -0300

    WIP Adiciona as funções de gerar números aleatórios, converter em Int e criar o array de cartas baseada no peso

--- app/core/Bot.hs
+++ app/core/Bot.hs
@@ -5,42 +5,65 @@
 module Bot where
 
 import Card
-import System.Random (randomRIO)
+import System.Random (randomRIO, Random (random))
+import GHC.IO (unsafeDupablePerformIO)
+
+-- Gera um número aleatório de 0 até b, recebido como parâmetro
+nRandom :: Int -> Int
+nRandom b = do
+    let rand = randomRIO (0,b) :: IO Int
+    typeConvert rand
+
+-- Converte de tipo IO Int para tipo Int
+typeConvert :: IO Int -> Int
+typeConvert = unsafeDupablePerformIO
+
+-- Recebe uma lista de cartas e uma lista de pesos e gera uma lista maior, onde cada carta é repetida de acordo com o peso
+wHand :: [a] -> [Int] -> [a]
+wHand [] [] = []
+wHand (x:xs) (y:ys) = replicate y x ++ wHand xs ys
+
+makeChoice :: [a] -> [Int] -> a
+makeChoice cards weight = do
+    hand <- wHand cards weight
+    let len = length hand
+    choice = nRandom len
+
 
 -- Faz a seleção da carta
-makechoice :: [Card] -> [Int] -> Card
-make_choice hand weight = do
-    let w_hand = weight_hand 0 hand weight
-        len = length w_hand
+-- makechoice :: [Card] -> [Int] -> Card
+-- make_choice hand weight = do
+--     let w_hand = weight_hand 0 hand weight
+--         len = length w_hand
 
-    return (hand !! (randomRIO (0, len - 1)))
+--     return (hand !! (randomRIO (0, len - 1)))
 
--- Gera um array com as cartas baseado no array de pesos. Se o peso de uma carta é 5, terão 5 ocorrências dessa carta no array
-weight_hand :: Int -> [Card] -> [Int] -> [Int]
-weight_hand i [] [] = []
-weight_hand 5 _ _ = []
-weight_hand i cards weights =
-    case elem of
-        "F" -> replicate (weights !! 0) i ++ weight_hand (i + 1) cards weights
-        "A" -> replicate (weights !! 1) i ++ weight_hand (i + 1) cards weights
-        "M" -> replicate (weights !! 2) i ++ weight_hand (i + 1) cards weights
-        "N" -> replicate (weights !! 3) i ++ weight_hand (i + 1) cards weights
-        "T" -> replicate (weights !! 4) i ++ weight_hand (i + 1) cards weights
-  where elem = element (cards !! i)
+-- -- Gera um array com as cartas baseado no array de pesos. Se o peso de uma carta é 5, terão 5 ocorrências dessa carta no array
+-- weight_hand :: Int -> [Card] -> [Int] -> [Int]
+-- weight_hand i [] [] = []
+-- weight_hand 5 _ _ = []
+-- weight_hand i cards weights =
+--     case elem of
+--         "F" -> replicate (weights !! 0) i ++ weight_hand (i + 1) cards weights
+--         "A" -> replicate (weights !! 1) i ++ weight_hand (i + 1) cards weights
+--         "M" -> replicate (weights !! 2) i ++ weight_hand (i + 1) cards weights
+--         "N" -> replicate (weights !! 3) i ++ weight_hand (i + 1) cards weights
+--         "T" -> replicate (weights !! 4) i ++ weight_hand (i + 1) cards weights
+--   where elem = element (cards !! i)
 
--- Retorna um deck com os mesmos elementos do deck passado, mas em uma ordem aleatória
-shuffle :: [a] -> IO [a]
-shuffle [] = []
-shuffle xs = do
-    let len = length xs
+-- -- Retorna um deck com os mesmos elementos do deck passado, mas em uma ordem aleatória
+-- shuffle :: [a] -> IO [a]
+-- shuffle [] = []
+-- shuffle xs = do
+--     let len = length xs
 
-    idx <- randomRIO (0, len - 1)
-    let val = xs !! idx
-    return ([val] ++ shuffle (removeFirstEqualTo val xs))
+--     idx <- randomRIO (0, len - 1)
+--     let val = xs !! idx
+--     return ([val] ++ shuffle (removeFirstEqualTo val xs))
 
--- Remove a primeira aparição do elemento passado do deck
-removeFirstEqualTo :: Eq a => a -> [a] -> [a]
-removeFirstEqualTo  [] = []
-removeFirstEqualTo x (y:ys)
-    | x == y    = ys
-    | otherwise = [y]  removeFirstEqualTo x ys
+-- -- Remove a primeira aparição do elemento passado do deck
+-- removeFirstEqualTo :: Eq a => a -> [a] -> [a]
+-- removeFirstEqualTo  [] = []
+-- removeFirstEqualTo x (y:ys)
+--     | x == y    = ys
+--     | otherwise = [y]  removeFirstEqualTo x ys
