705c76183139b557de760f66acc156b5a87c00ef
losout0 <leandro.oliveira.souto@ccc.ufcg.edu.br>
2024-03-27 20:10:14 -0300

    Removendo comentários desnecessários

--- app/core/Bot.hs
+++ app/core/Bot.hs
@@ -23,33 +23,12 @@ wHand :: [a] -> [Int] -> [a]
 wHand [] [] = []
 wHand (x:xs) (y:ys) = replicate y x ++ wHand xs ys
 
+-- Recebe uma lista de cartas e uma lista de pesos e consegue fazer a escolha da carta de acordo com os pesos
 makeChoice :: [a] -> [Int] -> a
 makeChoice cards weight = do
     let hand = wHand cards weight
     let len = length (wHand cards weight)
     hand !! nRandom (len -1)
-
-
--- Faz a seleção da carta
--- makechoice :: [Card] -> [Int] -> Card
--- make_choice hand weight = do
---     let w_hand = weight_hand 0 hand weight
---         len = length w_hand
-
---     return (hand !! (randomRIO (0, len - 1)))
-
--- -- Gera um array com as cartas baseado no array de pesos. Se o peso de uma carta é 5, terão 5 ocorrências dessa carta no array
--- weight_hand :: Int -> [Card] -> [Int] -> [Int]
--- weight_hand i [] [] = []
--- weight_hand 5 _ _ = []
--- weight_hand i cards weights =
---     case elem of
---         "F" -> replicate (weights !! 0) i ++ weight_hand (i + 1) cards weights
---         "A" -> replicate (weights !! 1) i ++ weight_hand (i + 1) cards weights
---         "M" -> replicate (weights !! 2) i ++ weight_hand (i + 1) cards weights
---         "N" -> replicate (weights !! 3) i ++ weight_hand (i + 1) cards weights
---         "T" -> replicate (weights !! 4) i ++ weight_hand (i + 1) cards weights
---   where elem = element (cards !! i)
 
 -- -- Retorna um deck com os mesmos elementos do deck passado, mas em uma ordem aleatória
 -- shuffle :: [a] -> IO [a]
