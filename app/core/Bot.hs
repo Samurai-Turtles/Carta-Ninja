b3ea12b5e4a4c8021f9cd460225739f451336c3f
losout0 <leandro.oliveira.souto@ccc.ufcg.edu.br>
2024-03-28 09:43:28 -0300

    Adicionando comentários

--- app/core/Bot.hs
+++ app/core/Bot.hs
@@ -8,15 +8,18 @@ import Card
 import System.Random
 
 -- Gera um número aleatório a partir da seed até o numero max, recebidos como parâmetro
+-- Semente -> Limite superior -> Numero aleatório
 nRandom :: Int -> Int -> Int
 nRandom seed max =  mod (31237 * seed) max
 
 -- Recebe uma lista de cartas e uma lista de pesos e gera uma lista maior, onde cada carta é repetida de acordo com o peso
+-- Lista de cartas -> Lista de pesos -> Lista maior de cartas
 wHand :: [a] -> [Int] -> [a]
 wHand [] [] = []
 wHand (x:xs) (y:ys) = replicate y x ++ wHand xs ys
 
 -- Recebe uma lista de cartas e uma lista de pesos e consegue fazer a escolha da carta de acordo com os pesos
+-- Pontuação do jogador -> Lista de Cartas -> Lista de Pesos -> Carta
 makeChoice :: Int -> [a] -> [Int] -> a
 makeChoice points cards weight = do
     let hand = wHand cards weight
@@ -29,10 +32,3 @@ shuffle [] = return []
 shuffle xs = do randomPosition <- getStdRandom (randomR (0, length xs - 1))
                 let (left, (a:right)) = splitAt randomPosition xs
                 fmap (a:) (shuffle (left ++ right))
-
--- Remove a primeira aparição do elemento passado do deck
--- removeFirstEqualTo :: Eq a => a -> [a] -> [a]
--- removeFirstEqualTo  [] = []
--- removeFirstEqualTo x (y:ys)
---     | x == y    = ys
---     | otherwise = [y]  removeFirstEqualTo x ys
