379151967ed98ffe64af3792854b603c730d5c04
sergio-gustavo-andrade-grilo <sergio.gustavo.andrade.grilo@ccc.ufcg.edu.br>
2024-03-29 15:29:32 -0300

    feat: adiciona formatação de Rankings para exibição em tela

--- app/models/Ranking.hs
+++ app/models/Ranking.hs
@@ -37,3 +37,8 @@ instance Eq Ranking where
 instance Ord Ranking where
     compare :: Ranking -> Ranking -> Ordering
     compare a b = points a `compare` points b
+
+formatRankingToScreen :: Ranking -> String
+formatRankingToScreen r =
+    if length (name r) < 20 then take (20 - length (name r)) (cycle " ") ++ name r ++ take (3 - length (show (points r))) (cycle " ") ++ show (points r)
+    else take 20 (name r) ++ take (3 - length (show (points r))) (cycle " ") ++ show (points r)
