a608859e772d2fd23d93c3bbe3ccb296520ee0f2
JWillianSl <jose.williansl@hotmail.com>
2024-03-30 16:01:51 -0300

    build: adiciona valor de módulo `GameLoop` ao `.cabal`

--- app/Main.hs
+++ app/Main.hs
@@ -13,6 +13,10 @@ import Ranking
 import Render (action)
 import Ranking (Ranking(Ranking))
 
+
+-- imports permanentes:: 
+import GameLoop
+
 -- testGetWinner :: IO ()
 -- testGetWinner = do
 --     let card1 = Card 1 "fire" 10
@@ -41,6 +45,7 @@ import Ranking (Ranking(Ranking))
 
 main :: IO ()
 main = do
+    initLoop
     -- let c1 = Card 01 "fire" 1
     -- let c2 = Card 02 "water" 2
     -- let list = [c1, c2]
@@ -89,36 +94,36 @@ main = do
     -- testUpdatePlayerCP
 
     -- UI TESTS::::::::
-    c <- getLine
-    --
-    if c /= "fim" then do
-        writeGlobalState $ GlobalState {screen = c, rankings = []}
-        --defineDeck
-        action
-        main
-        else print "fim"
+--     c <- getLine
+--     --
+--     if c /= "fim" then do
+--         writeGlobalState $ GlobalState {screen = c, rankings = []}
+--         --defineDeck
+--         action
+--         main
+--         else print "fim"
 
-defineDeck :: IO()
-defineDeck = do
-    let deck = drop 5 getCards
-    currentData <- getBattleState
+-- defineDeck :: IO()
+-- defineDeck = do
+--     let deck = drop 5 getCards
+--     currentData <- getBattleState
 
-    let modifiedData = 
-            BattleState { 
-                currentRound = currentRound currentData,
-                specialDeck = ["swapInDeck", "swapBetweenDecks"],
-                specialCardInUse = True,
+--     let modifiedData = 
+--             BattleState { 
+--                 currentRound = currentRound currentData,
+--                 specialDeck = ["swapInDeck", "swapBetweenDecks"],
+--                 specialCardInUse = True,
 
-                playerScore = 5,
-                playerStreak = playerStreak currentData, 
-                playerWinsByElement = [True, True, True, True, True],
-                playerDeck = deck,
+--                 playerScore = 5,
+--                 playerStreak = playerStreak currentData, 
+--                 playerWinsByElement = [True, True, True, True, True],
+--                 playerDeck = deck,
 
-                cpuScore = 98,
-                cpuStreak = cpuStreak currentData, 
-                cpuWinsByElement = cpuWinsByElement currentData,
-                cpuDeck = cpuDeck currentData
-            }
+--                 cpuScore = 98,
+--                 cpuStreak = cpuStreak currentData, 
+--                 cpuWinsByElement = cpuWinsByElement currentData,
+--                 cpuDeck = cpuDeck currentData
+--             }
 
-    writeBattleState modifiedData
+--     writeBattleState modifiedData
 
