1426497b19f2067a82568c3bc33d0eb2d676f010
ViniciusUltraNX <115042218+ViniciusUltraAD@users.noreply.github.com>
2024-04-25 08:38:49 -0300

    Slight refactoring on getWinner predicates.

--- app/core/Gameplay.pl
+++ app/core/Gameplay.pl
@@ -9,39 +9,39 @@ getWinner(card(id(IdP), elem(ElemP), power(PowerP)), card(id(IdC), elem(ElemC), 
     getWinnerByPower(card(id(IdP), elem(ElemP), power(PowerP)), card(id(IdC), elem(ElemC), power(PowerC)), O),
     R is O,
     !.
-getWinner(card(id(), elem("fire"), power()), card(id(), elem(ElemC), power()), R) :-
+getWinner(card(id(_), elem("fire"), power(_)), card(id(_), elem(ElemC), power(_)), R) :-
     (ElemC = "nature" ; ElemC = "metal"),
     R is 1,
     !.
-getWinner(card(id(), elem("metal"), power()), card(id(), elem(ElemC), power()), R) :-
+getWinner(card(id(_), elem("metal"), power(_)), card(id(_), elem(ElemC), power(_)), R) :-
     (ElemC = "nature" ; ElemC = "earth"),
     R is 1,
     !.
-getWinner(card(id(), elem("nature"), power()), card(id(), elem(ElemC), power()), R) :-
+getWinner(card(id(_), elem("nature"), power(_)), card(id(_), elem(ElemC), power(_)), R) :-
     (ElemC = "earth" ; ElemC = "water"),
     R is 1,
     !.
-getWinner(card(id(), elem("earth"), power()), card(id(), elem(ElemC), power()), R) :-
+getWinner(card(id(_), elem("earth"), power(_)), card(id(_), elem(ElemC), power(_)), R) :-
     (ElemC = "fire" ; ElemC = "water"),
     R is 1,
     !.
-getWinner(card(id(), elem("water"), power()), card(id(), elem(ElemC), power()), R) :-
+getWinner(card(id(_), elem("water"), power(_)), card(id(_), elem(ElemC), power(_)), R) :-
     (ElemC = "fire" ; ElemC = "metal"),
     R is 1,
     !.
-getWinner(card(id(), elem(), power()), card(id(), elem(), power()), -1).
+getWinner(card(id(_), elem(_), power(_)), card(id(_), elem(_), power(_)), -1).
 
 
 /* Predicado que verifica qual carta dada ganhou a rodada a partir de uma comparação
  do poder de ambas, retornando 1 para o caso em que o jogador ganha, -1 quando o bot ganha
  e 0 para quando há empate.
  */
-getWinnerByPower(card(id(), elem(), power(PowerP)), card(id(), elem(), power(PowerC)), R) :-
+getWinnerByPower(card(id(_), elem(_), power(PowerP)), card(id(_), elem(_), power(PowerC)), R) :-
     (PowerP > PowerC),
     R is 1,
     !.
-getWinnerByPower(card(id(), elem(), power(PowerP)), card(id(), elem(), power(PowerC)), R) :-
+getWinnerByPower(card(id(_), elem(_), power(PowerP)), card(id(_), elem(_), power(PowerC)), R) :-
     (PowerP < PowerC),
     R is -1,
     !.
-getWinnerByPower(card(id(), elem(), power()), card(id(), elem(), power()), 0).
+getWinnerByPower(card(id(_), elem(_), power(_)), card(id(_), elem(_), power(_)), 0).
