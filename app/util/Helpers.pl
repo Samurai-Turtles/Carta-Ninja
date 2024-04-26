5f14a7b1dd1d7db1377c22ee2ba704b695a1472a
ViniciusUltraNX <115042218+ViniciusUltraAD@users.noreply.github.com>
2024-04-26 08:40:45 -0300

    Add Helpers.pl and some predicates inside of it
    This file has the role of receiving general predicates and others that
    abstract some functionalities for other predicates, by now it has a
    predicate to get an element from a card and two for putting a given card
    to the end of the deck.

---
+++ app/util/Helpers.pl
@@ -0,0 +1,32 @@
+/* Esse predicado verifica casos de exceção quanto à
+ carta jogada antes de realmente colocá-la no final
+ do deck
+ */
+pushCardToEnd(_, [], []) :- !.
+pushCardToEnd(_, [X], [X]) :- !.
+pushCardToEnd(Idx, Deck, ReturnedDeck) :-
+    length(Deck, SizeOfDeck),
+    (Idx < 0 ; Idx >= SizeOfDeck),
+    ReturnedDeck = Deck,
+    !.
+pushCardToEnd(Idx, Deck, ReturnedDeck) :-
+    elementToEnd(Idx, Deck, R),
+    ReturnedDeck = R.
+
+/* Predicado que dado um índice válido e uma lista pega o 
+ elemento desse índice e move para o final, retornando 
+ essa lista com essa modificação.
+*/
+elementToEnd(Idx, [H|T], ReturnedList) :-
+    (Idx =:= 0),
+    append(T, [H], R),
+    ReturnedList = R,
+    !.
+elementToEnd(Idx, [H|T], ReturnedList) :-
+    NewIdx is Idx - 1,
+    elementToEnd(NewIdx, T, R1),
+    append([H], R1, R),
+    ReturnedList = R.
+
+% Dado uma carta, retorne o elemento dela.
+getElem(card(id(_), elem(Element), power(_)), Element).
