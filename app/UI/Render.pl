5b7d2be7c6247b6c839dff4d54697c456ce0ba1c
Sérgio Gustavo <sergio.gustavo.andrade.grilo@ccc.ufcg.edu.br>
2024-04-27 13:01:33 -0300

    chore: muda case para snake case

--- app/UI/Render.pl
+++ app/UI/Render.pl
@@ -4,35 +4,35 @@
 action :-
     shell(clear), % Como verificar qual é o sistema operacional?
     % pegar o state atual 
-    selectDraw(menu). % Chamar o selectDraw para o state.
+    select_draw(menu). % Chamar o select_draw para o state.
 
 /*
     Esta função seleciona a função `draw` responsável pela impressão da tela, 
 tomado por base o estado atual.
 */
-selectDraw(StateScreen) :-
-    atom_string("menu", StateScreen) -> drawMenu;
-    atom_string("ranking", StateScreen) -> drawRanking;
-    atom_string("creditos", StateScreen) -> drawCreditos;
-    atom_string("desafiante", StateScreen) -> drawDesafiante;
-    atom_string("batalha", StateScreen) -> drawBatalha;
-    atom_string("comparacao", StateScreen) -> drawComparacao;
-    atom_string("vitoria", StateScreen) -> drawVitoria;
-    atom_string("derrota", StateScreen) -> drawDerrota;
-    atom_string("empate", StateScreen) -> drawEmpate;
-    atom_string("gameOver", StateScreen) -> drawGameOver;
-    atom_string("gameClear", StateScreen) -> drawGameClear;
+select_draw(StateScreen) :-
+    atom_string("menu", StateScreen) -> draw_menu;
+    atom_string("ranking", StateScreen) -> draw_ranking;
+    atom_string("creditos", StateScreen) -> draw_creditos;
+    atom_string("desafiante", StateScreen) -> draw_desafiante;
+    atom_string("batalha", StateScreen) -> draw_batalha;
+    atom_string("comparacao", StateScreen) -> draw_comparacao;
+    atom_string("vitoria", StateScreen) -> draw_vitoria;
+    atom_string("derrota", StateScreen) -> draw_derrota;
+    atom_string("empate", StateScreen) -> draw_empate;
+    atom_string("gameOver", StateScreen) -> draw_game_over;
+    atom_string("gameClear", StateScreen) -> draw_game_clear;
     string_concat("StateScreen not identified: ", StateScreen, R),
     write(R), writeln(" does not exist.").
 
 % Esta função imprime a tela de menu.
-drawMenu :- 
+draw_menu :- 
     screen("menu", Screen), 
     unlines(Screen, "\n", Result),
     writeln(Result).
 
 % Esta função imprime a tela de ranking.
-drawRank :-
+draw_ranking :-
     screen("ranking", Screen), 
     % Pegar o valor do estado global
     % Formatar os primeiros 6 rankings na tela
@@ -51,7 +51,7 @@ drawRank :-
     length(RepCompl, RepComplLength),
     LengthWorkaround is RepComplLength - 1,
 
-    mergeControll([RepCompl], LengthWorkaround, Controll),
+    merge_controll([RepCompl], LengthWorkaround, Controll),
     /*
         [LEGADO] 
     unlines(Controll, "", ControllUnlines),
@@ -63,10 +63,10 @@ drawRank :-
     forgeScreen(ScreenUnlinesChars, ControllUnlinesChars, RankScr),
     */
     anvil(Screen, Controll, RankScr),
-    printList(RankScr).
+    print_list(RankScr).
     
 % Esta função imprime a tela de créditos.
-drawCreditos :- 
+draw_creditos :- 
     screen("creditos", Screen), 
     unlines(Screen, "\n", Result),
     writeln(Result).
@@ -75,13 +75,13 @@ drawCreditos :-
 Esta função imprime a tela do desafiante, destinada a pedir
 que o desafiante escrevra seu nome.
 */
-drawDesafiante :- 
+draw_desafiante :- 
     screen("desafiante", Screen), 
     unlines(Screen, "\n", Result),
     writeln(Result).
 
 % Esta função imprime a tela de batalha.
-drawBatalha :-
+draw_batalha :-
     % Pegar o valor do estado de batalha
     get_player_state(PlayerData),
     % Pegar o valor do estado da campanha
@@ -90,7 +90,7 @@ drawBatalha :-
     nth0(2, PlayerData, PlayerDeck),
 
     % Pegar a representação das cartas do jogador
-    currentCards(0, PlayerDeck, PlayerHandRep),
+    current_cards(0, PlayerDeck, PlayerHandRep),
 
     % Pegar as cartas especiais
     
@@ -98,22 +98,22 @@ drawBatalha :-
     !.
 
 % Esta função imprime a tela de Comparação entre cartas.
-drawCompare :- !.
+draw_comparacao :- !.
 
 % Esta função imprime a tela de vitória.
-drawVenceu :- !.
+draw_vitoria :- !.
 
 % Esta função imprime a tela de derrota.
-drawDerrota :- !.
+draw_derrota :- !.
 
 % Esta função imprime a tela de empate.
-drawEmpate :- !.
+draw_empate :- !.
 
 % Esta função imprime a tela de gameOver
-drawGameOver :- !.
+draw_game_over :- !.
 
 % Esta função imprime a tela de GameClear.
-drawGameClear :- !.
+draw_game_clear :- !.
 
 % Funções Auxiliares: mandar para um arquivo utils
 
@@ -125,39 +125,39 @@ repeat(Str,Num,Res):-
     string_concat(Str, Res1, Res).
 
 % TODO Apagar depois
-printList([]):-!.
-printList([H | T]) :-
+print_list([]):-!.
+print_list([H | T]) :-
     write(H),
-    printList(T).
+    print_list(T).
 
 % Chamar com 0 quando for usar
 % Testar se funciona mesmo
-currentCards(ListIndex, _, []) :- ListIndex >= 6, !.
-currentCards(ListIndex, [HeadCardList | TailCardList], RepresentationList) :- 
+current_cards(ListIndex, _, []) :- ListIndex >= 6, !.
+current_cards(ListIndex, [HeadCardList | TailCardList], RepresentationList) :- 
     % Pegar a carta do head (?)
     HeadCardList = card(id(IdCard), elem(_), power(_)), 
 
     % Pega a representação da carta correspondente ao IdCard
-    cardRep(CardRepresentations),
-    nth0(IdCard, CardRepresentations, CurrCard),
+    card_rep(card_representations),
+    nth0(IdCard, card_representations, CurrCard),
 
     % Chamada recursiva.
     NewListIndex is ListIndex + 1,
-    currentCards(NewListIndex, TailCardList, RecursiveList),
+    current_cards(NewListIndex, TailCardList, RecursiveList),
     
     % Juntando numa lista só
     append([CurrCard], RecursiveList, RepresentationList). 
 
-specialArr(_, SpecialCardDeck, ["", "6", "", "7", "", "8", "", ""]) :- length(SpecialCardDeck, 3), !.
-specialArr(SpecialCardInUse, SpecialCardDeck, Output) :-
+special_card_list(_, SpecialCardDeck, ["", "6", "", "7", "", "8", "", ""]) :- length(SpecialCardDeck, 3), !.
+special_card_list(SpecialCardInUse, SpecialCardDeck, Output) :-
     % Acho que para ver se é true é assim
     SpecialCardInUse = true,
     length(SpecialCardDeck, 2),
-    specialCheckUse(SpecialCardDeck, Output),
+    special_card_check(SpecialCardDeck, Output),
     !.
-specialArr(_, _, ["", "X", "", "X", "", "X", "", ""]).
+special_card_list(_, _, ["", "X", "", "X", "", "X", "", ""]).
 
-specialCheckUse(Specials, SpecialCheck) :-
+special_card_check(Specials, SpecialCheck) :-
     % Tenho certeza de que tem um jeito melhor de fazer isso
     (member("swapInDeck", Specials) -> SwapInDeck = "X" ; SwapInDeck = ">"),
     (member("nullifyElement", Specials) -> NullifyElement = "X" ; NullifyElement = ">"),
