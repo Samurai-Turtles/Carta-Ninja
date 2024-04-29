ffeb1c85654985d56791a62aaaa1d1f0eb1067c2
Douglas Domingos <doug.domingossilva@gmail.com>
2024-04-28 22:18:37 -0300

    feat: finaliza predicados de Ranking

--- app/core/Ranking.pl
+++ app/core/Ranking.pl
@@ -1,5 +1,4 @@
 % Predicados para gerenciamento de arquivos CSV
-
 :- use_module(library(csv)).
 
 /*
@@ -7,15 +6,29 @@
  */
 read_ranking(Data) :-
     get_path("ranking.csv", Path),
-    csv_read_file(Path, Data, [functor(row)]).
+    csv_read_file(Path, Rows, [functor(row)]),
+    parse_rows(Rows, Data).
 
 /*
  * Adiciona um novo registro de Rank ao arquivo.
  */
 add_rank(RankName, RankPts) :-
-    read_ranking(CSV),
     get_path("ranking.csv", Path),
-    csv_write_file(Path, [row(RankName, RankPts) | CSV]).
+    read_ranking(RankingCSV),
+    update_rank_list(RankName, RankPts, RankingCSV, TMP),
+    parse_rows(TMP, NewRankingCSV),
+    csv_write_file(Path, NewRankingCSV).
+
+/*
+ * Atualiza a lista de Rankings com um novo registro.
+ * 
+ * Se o nome do novo registro está presente na lista, sua pontuação será atualizada.
+ * Caso contrário, o registro será adicionado como uma nova lista.
+ */
+update_rank_list(RankName, RankPts, [], [[RankName, RankPts]]).
+update_rank_list(RankName, RankPts, [[RankName | _] | Rows], [[RankName, RankPts] | Rows]) :- !.
+update_rank_list(RankName, RankPts, [[X, Y] | Rows], [[X, Y] | NextRows]) :-
+    update_rank_list(RankName, RankPts, Rows, NextRows).
 
 /*
  * Retorna o Path do arquivo de Rankings.
@@ -25,14 +38,15 @@ get_path(Filename, Path) :-
     atomic_list_concat([CWD, 'app/data/', Filename], Path).
 
 /*
- * Converte os dados CSV para uma lista de listas, onde
- * cada lista representa uma linha do arquivo.
+ * Converte os dados CSV em uma lista de listas e vice-versa.
  */
 parse_rows(Rows, Lists) :-
     maplist(row_to_list, Rows, Lists).
 
 /*
- * Converte uma linha CSV em uma lista.
+ * Converte uma linha CSV em uma lista e vice-versa.
  */
 row_to_list(Row, List) :-
+    Row =.. [row|List].
+row_to_list(List, Row) :-
     Row =.. [row|List].
