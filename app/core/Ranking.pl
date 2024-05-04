84f0998f956eba8acc5aed29006d2ef5a7bf8f85
Douglas Domingos <doug.domingossilva@gmail.com>
2024-05-04 03:13:17 -0300

    fix: adiciona conversões atom-string na leitura e escrita do CSV

--- app/core/Ranking.pl
+++ app/core/Ranking.pl
@@ -2,35 +2,58 @@
 :- use_module(library(csv)).
 
 /*
- * Retorna os dados do Ranking.
+ * Retorna os dados do Ranking em uma lista de tuplas (listas),
+ * onde cada tupla segue o formato [Rank, Points].
  */
 read_ranking(Data) :-
     get_ranking_filepath(Path),
     csv_read_file(Path, Rows, [functor(row)]),
-    parse_rows(Rows, Data), !.
+    parse_rows(Rows, Data),
+	!.
 
 /*
- * Adiciona um novo registro de Rank ao arquivo.
+ * Recebe os dados de uma campanha e as grava no Ranking. A
+ * lista é gravada de forma ordenada e decrescente.
  */
-add_rank(RankName, RankPts) :-
-    get_ranking_filepath(Path),
+add_rank(Name, Points) :-
+    % Puxa os dados existentes no CSV
     read_ranking(RankingCSV),
-    atom_string(RankNameAtom, RankName),
-    update_rank_list(RankNameAtom, RankPts, RankingCSV, TMP1),
+
+    % Adiciona os novos dados à lista de ranks
+    atom_string(Name, ParsedName),
+    insert_rank(ParsedName, Points, RankingCSV, TMP1),
+    
+    % Ordena a lista de ranks e a converte para linhas CSV
     sort_rankings(TMP1, TMP2),
     parse_rows(TMP2, NewRankingCSV),
-    csv_write_file(Path, NewRankingCSV), !.
+
+    % Escreve a lista atualizada no arquivo
+    get_ranking_filepath(Path),
+    csv_write_file(Path, NewRankingCSV),
+	!.
+
+% ====================== Auxiliares ====================== %
 
 /*
- * Atualiza a lista de Rankings com um novo registro.
- * 
- * Se o nome do novo registro está presente na lista, sua pontuação será atualizada.
- * Caso contrário, o registro será adicionado como uma nova lista.
+ * Recebe os dados da campanha a ser gravada na lista e retorna a
+ * nova lista com os dados inseridos.
+ *
+ * O registro da campanha pode ocorrer de duas maneiras:
+ *
+ *  1º) Existe uma campanha com mesmo nome na lista:
+ *      permanece a maior pontuação dentre os registros
+ *  2º) Não existe uma campanha com o mesmo nome na lista:
+ *      uma nova tupla é adicionada ao final da lista *
+ *
  */
-update_rank_list(RankName, RankPts, [], [[RankName, RankPts]]) :- !.
-update_rank_list(RankName, RankPts, [[RankName | _] | Rows], [[RankName, RankPts] | Rows]) :- !.
-update_rank_list(RankName, RankPts, [[X, Y] | Rows], [[X, Y] | NextRows]) :-
-    update_rank_list(RankName, RankPts, Rows, NextRows).
+insert_rank(Name, Points, [], [[Name, Points]]) :- !.
+insert_rank(Name, Points, [[Name, RankPts] | List], [[Name, X] | List]) :-
+    ( Points > RankPts
+    -> X = Points
+    ; X = RankPts
+    ), !.
+insert_rank(Name, Points, [Rank | Rows], [Rank | NextRows]) :-
+    insert_rank(Name, Points, Rows, NextRows).
 
 /*
  * Ordena a lista de rankings em ordem decrescente. 
@@ -48,18 +71,21 @@ compare_ranks(Order, [_, A], [_, B]) :-
     ).
 
 /*
- * Retorna o Path do arquivo de Rankings.
+ * Retorna o caminho absoluto do arquivo de Ranking.
  */
 get_ranking_filepath(Path) :-
+    % Pega o caminho absoluto do diretório atual e converte-o em uma lista
     working_directory(WorkDir, WorkDir),
     split_string(WorkDir, "/", "/", TMP1),
+
+    % Concatena o caminho do arquivo CSV ao caminho do diretório raíz do projeto
     get_project_path("app", TMP1, ProjectDir),
     atomic_list_concat(["/", ProjectDir, "app/data/ranking.csv"], TMP2),
     atom_string(TMP2, Path),
-    !.
+	!.
 
 /*
- * Retorna o Path absoluto do projeto.
+ * Retorna o caminho absoluto do diretório raíz do projeto.
  */
 get_project_path(_, [], "").
 get_project_path(DirName, [DirName | _], "") :- !.
@@ -72,12 +98,24 @@ get_project_path(DirName, [H | T], Path) :-
  * Converte os dados CSV em uma lista de listas e vice-versa.
  */
 parse_rows(Rows, Lists) :-
-    maplist(row_to_list, Rows, Lists).
+    maplist(row_to_list, Rows, TMP),
+    maplist(name_to_string, TMP, Lists).
+parse_rows(Lists, Rows) :-
+    maplist(name_to_string, TMP, Lists),
+    maplist(row_to_list, Rows, TMP).
+
+/*
+ * Converte o nome do Rank (atom) para uma string e vice-versa.
+ */
+name_to_string([Name, Points], [String, Points]) :- 
+    atom_string(Name, String), !.
+name_to_string([String, Points], [Name, Points]) :-
+    atom_string(String, Name), !.
 
 /*
  * Converte uma linha CSV em uma lista e vice-versa.
  */
 row_to_list(Row, List) :-
-    Row =.. [row|List].
+    Row =.. [row | List].
 row_to_list(List, Row) :-
-    Row =.. [row|List].
+    Row =.. [row | List].
